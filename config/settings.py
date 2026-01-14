e37f0280ce365ae2d785e42267dd1f66794df0a2
Abhilove Goyal <140530756+Abhilove-Goyal@users.noreply.github.com>
2026-01-15 01:19:02 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-        extra = "forbid"
+        extra = "ignore"
 
 
 def get_settings():
